2c45e39a5dbc5048da38ab4196bb450c45bc1d47
rana <5546ranageorge@gmail.com>
2024-05-30 18:32:14 +0530

    edited login file as index files and chaned index files as main files

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     // Simple client-side validation (you should also validate on the server-side)
     if (username === 'r14rana' && password === 'elonmusk') {
         // Redirect to the desired page
-        window.location.href = 'main/index.html'; // Change 'dashboard/dashboard.html' to your desired page path
+        window.location.href = 'main/main.html'; // Change 'dashboard/dashboard.html' to your desired page path
     } else {
         document.getElementById('message').innerText = 'Invalid username or password.';
         document.getElementById('message').style.color = 'red';
